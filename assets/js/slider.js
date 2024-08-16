2af21f6115d5e2482e0dacb620f82be090f98c93
Omer Faruk KUMRAL <the.kumral44@gmail.com>
2024-08-16 16:20:39 +0300

    automatic slider added and fixed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,33 +2,41 @@ let slideIndex = 0;
 
 function showSlides() {
   let slides = document.querySelectorAll('.slider img');
-  slides.forEach((slide, index) => {
-    slide.style.display = 'none'; // Hide all slides
-  });
+  if (slides.length > 0) {
+    slides.forEach((slide) => (slide.style.display = 'none')); // Hide all slides
 
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = 'block'; // Show current slide
   }
-
-  slides[slideIndex - 1].style.display = 'block'; // Show current slide
 }
 
-// Next/previous controls
+// Initialize the slider
+function initSlider() {
+  showSlides(); // Show the first slide
+  setInterval(showSlides, 5000); // Change slide every 5 seconds
+
+  document
+    .querySelector('.prev')
+    .addEventListener('click', () => plusSlides(-1));
+  document
+    .querySelector('.next')
+    .addEventListener('click', () => plusSlides(1));
+}
+
 function plusSlides(n) {
   slideIndex += n;
   let slides = document.querySelectorAll('.slider img');
-  if (slideIndex > slides.length) slideIndex = 1;
-  if (slideIndex < 1) slideIndex = slides.length;
-  slides.forEach((slide) => (slide.style.display = 'none'));
-  slides[slideIndex - 1].style.display = 'block';
+  if (slides.length > 0) {
+    if (slideIndex > slides.length) slideIndex = 1;
+    if (slideIndex < 1) slideIndex = slides.length;
+    slides.forEach((slide) => (slide.style.display = 'none'));
+    slides[slideIndex - 1].style.display = 'block';
+  }
 }
 
-// Attach event listeners
-document.querySelector('.prev').addEventListener('click', function () {
-  plusSlides(-1);
-});
-
-document.querySelector('.next').addEventListener('click', function () {
-  plusSlides(1);
-});
+// Attach event listeners once the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', initSlider);
